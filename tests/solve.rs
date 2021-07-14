23c137898d81ed8edc58683f860d949d3ac0b721
Alex Kornitzer <alex.kornitzer@f-secure.com>
2021-07-14 17:45:04 +0100

    temp: track changes

--- tests/solve.rs
+++ tests/solve.rs
@@ -12,5 +12,28 @@ macro_rules! solve_rule {
     };
 }
 
-solve_rule!("boolean_group");
+// Expressions
+solve_rule!("boolean");
+solve_rule!("boolean_group_and");
+solve_rule!("boolean_group_or");
+solve_rule!("boolean_expression_and");
+solve_rule!("boolean_expression_equal");
+solve_rule!("boolean_expression_greater_than");
+solve_rule!("boolean_expression_greater_than_or_equal");
+solve_rule!("boolean_expression_less_than");
+solve_rule!("boolean_expression_less_than_or_equal");
+solve_rule!("boolean_expression_or");
+solve_rule!("cast_int");
+solve_rule!("cast_str");
 solve_rule!("identifier");
+solve_rule!("integer");
+solve_rule!("match_and");
+solve_rule!("match_or");
+solve_rule!("negate");
+solve_rule!("nested");
+solve_rule!("nested_dot_notation");
+solve_rule!("search_contains");
+solve_rule!("search_ends_with");
+solve_rule!("search_exact");
+solve_rule!("search_regex");
+solve_rule!("search_starts_with");
